042e75fe44212b2771a408527554f8edcef2cea3
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-07-22 21:49:16 -0600

    remove code

--- app.js
+++ app.js
@@ -2,4 +2,18 @@
 * i3logix Code Challenge
 * 
 * Please refer to the README.md for challenge questions and complete your challenge below.
+
+Ranking Poker Hands
+
+Write code that will evaluate a poker hand and determine its
+rank.
+
+Example:
+
+Hand: Ah As 10 c 7 d 6 s(Pair of Aces)
+
+Hand: Kh Kc 3 s 3 h 2 d(2 Pair)
+
+Hand: Kh Qh 6 h 2 h 9 h(Flush)
+
 */
