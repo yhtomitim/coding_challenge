732470f1e3e98bbbd62fc809ed84ea5446d5a496
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-07-23 00:34:42 -0600

    add tests for checking if cards match hands

--- test/test.js
+++ test/test.js
@@ -1,9 +1,54 @@
 const assert = require('chai').assert;
 const application = require('../app.js');
 
-describe('Application', () => {
+describe('Poker Hand Evaluator', () => {
+  it('Checks that the argument passed in is array and has a length of 5', () => {
+    const hand = ['Ac', 'Ad', 'Ah', 'As', '5d'];
+    assert.isArray(hand, true);
+    assert.lengthOf(hand, 5);
+  });
+  it('Displays high hand message', () => {
+    const hand = ['10c', 'Ad', '2h', '4s', '5d'];
+    assert.deepEqual(application(hand), 'You have a High Card hand');
+  });
+  it('Displays pair hand message', () => {
+    const hand = ['10c', '10d', '2h', '4s', '5d'];
+    assert.deepEqual(application(hand), 'You have a Pair');
+  });
+  it('Displays two pair hand message', () => {
+    const hand = ['10c', '10d', '4h', '4s', '5d'];
+    assert.deepEqual(application(hand), 'You have Two Pair');
+  });
+  it('Displays three of a kind hand message', () => {
+    const hand = ['10c', '10d', '10h', '4s', '5d'];
+    assert.deepEqual(application(hand), 'You have Three of a Kind');
+  });
+  it('Displays straight hand message', () => {
+    const hand = ['7c', '8d', '10h', 'Js', '9d'];
+    assert.deepEqual(application(hand), 'You have a Straight');
+  });
+  it('Displays flush hand message', () => {
+    const hand = ['10c', 'Ac', '2c', '4c', '5c'];
+    assert.deepEqual(application(hand), 'You have a Flush');
+  });
+  it('Displays full house hand message', () => {
+    const hand = ['10c', 'Ac', '10h', '10d', 'As'];
+    assert.deepEqual(application(hand), 'You have a Full House');
+  });
+  it('Displays four of a kind hand message', () => {
+    const hand = ['10c', '10s', '10h', '10d', 'As'];
+    assert.deepEqual(application(hand), 'You have Four of a Kind');
+  });
+  it('Displays straight flush hand message', () => {
+    const hand = ['5c', '9c', '7c', '6c', '8c'];
+    assert.deepEqual(application(hand), 'You have a Straight Flush');
+  });
+   it('Displays royal flush hand message', () => {
+     const hand = ['Qs', 'Js', 'As', '10s', 'Ks'];
+     assert.deepEqual(application(hand), 'You have a Royal Flush');
+   });
+  it('Checks for unique values in the hand array', () => {
+    const hand = ['Ac', 'Ad', 'Ac', 'As', '5d'];
+  });
 
-  it('returns desired result', () => {
-    assert.deepEqual(application(arguments), 'desired result')
-  })
 });
